8d17808ce92564801a3368561cc3b3429b793490
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-09-03 16:19:41 +0200

    Add rendering logic: initialize camera, viewport, and pixel-ray generation loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,20 @@ import org.example.shapes.Triangle;
 
 void main() {
 
+    //================================Scene================================
     var scene = new Scene();
+
+    //=====================Bildinställningar=====================
+    //Pixel storlek
+    var imageWidth = 40;
+    var imageHeight = 20;
+
+    // Kamera, viewport och position
+    var viewportWidth = 4.0;
+    var viewportHeight = 2.0;
+    var cameraPosition = new Vector3(0, 0, 0);
+    //Avståndet från kameran till viewporten
+    var focalLength = 1.0;
 
 //================================Sphere=================================
     // Exempel på sphere
@@ -67,6 +80,33 @@ void main() {
             new Vector3(0, 0, 1)
     );
 
+
+   //=============================Loop genom bildens pixlar/Rendering logic=================================
+    for (int y = 0; y < imageHeight; y++) {
+        for (int x = 0; x < imageWidth; x++) {
+            // Varje pixel får sin egen (x, y)-koordinat, viewport-position och riktning, och därefter skapas en ray
+            // Omvandlar pixelkoordinaterna x och y till värden mellan 0.0 och 1.0
+            var u = (double) x / (imageWidth - 1);
+            var v = (double) y / (imageHeight - 1);
+
+            // Omvandlar u till x-koordinat i viewporten
+            var viewportX = (u - 0.5) * viewportWidth;
+            var viewportY = (0.5 - v) * viewportHeight;
+
+            //Att skapa ray riktningen från kameran genom pixelns punkt
+            var direction = new Vector3(
+                    viewportX,
+                    viewportY,
+                    focalLength // viewportens position framför kameran i z-led
+                    ).normalize();
+
+            // Att skapar en ray från kameran mot pixeln
+            var ray = new Ray(cameraPosition, direction);
+            // Att kontrollerar om pixelns ray träffar något objekt i scenen
+            var isHit = scene.hit(ray);
+
+        }
+    }
     //=============================Utskrivt_Sphere=================================
     IO.println("Test 1: " + sphere.hit(ray1));
     IO.println("Test 2: " + sphere.hit(ray2));
